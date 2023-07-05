b171686cb10e18d0a7ef9e51f43d799c0e9062e0
jweeeezy <jwillert@42heilbronn.student.de>
2023-07-05 17:34:08 +0200

    Fix: Small optimisation, works on school MAC now

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 
 int	main(int argc, char const **argv)
 {
+	if (argc < 0)
+	{
+		std::cout << "negative arguments!" << std::endl;
+	}
+	if (argv == NULL)
+	{
+		std::cout << "no arguments" << std::endl;
+	}
 	std::cout << "Hello World!" << std::endl;
 	return (0);
 }
